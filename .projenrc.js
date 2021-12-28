24e100b4a8118f25952167cb4a246e5ea3b21344
mmuller88 <damadden88@googlemail.com>
2021-12-28 10:58:04 +0100

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,16 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'mmuller88',
+  authorAddress: 'damadden88@googlemail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-neko',
+  repositoryUrl: 'https://github.com/mmuller88/cdk-neko',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // release: undefined,      /* Add release management to this project. */
+});
+project.synth();
